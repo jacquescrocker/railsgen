8b67da3a0bc86805e7087be14d3f026f64a2d92b
Jacques Crocker <railsjedi@gmail.com>
2010-03-10 09:34:04 -0800

    Additional fixes

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,7 +10,7 @@ module NavigationHelpers
     
     when /the home\s?page/
       '/'
-    
+      
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
