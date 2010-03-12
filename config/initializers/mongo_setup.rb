44e082f5681dab0adf6cd398f4aeadb0d61c10b7
Jacques Crocker <railsjedi@gmail.com>
2010-03-11 17:49:00 -0700

    Mongomapper config deploy

---
+++ config/initializers/mongo_setup.rb
@@ -0,0 +1,12 @@
+puts "WTF!!!"
+# MongoMapper.database = ENV["MONGOHQ_DATABASE"]
+MongoMapper.config = {
+  "production" => {
+    "host" => ENV["MONGOHQ_HOST"],
+    "port" => ENV["MONGOHQ_PORT"],
+    "database" => ENV["MONGOHQ_DATABASE"],
+    "username" => ENV["MONGOHQ_USERNAME"],
+    "password" => ENV["MONGOHQ_PASSWORD"]
+  }
+}
+MongoMapper.connect "production"
