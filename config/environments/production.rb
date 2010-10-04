93b58212f2ad00cdf20eb7a8e5bd0c802d5c2118
Jacques Crocker <railsjedi@gmail.com>
2010-10-04 15:46:23 -0700

    Don't update sass on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,4 +48,7 @@ RailsGenerate::Application.configure do
 
   config.active_support.deprecation = :log
 
+  # dont update sass on production (we're using static assets)
+  Sass::Plugin.options.merge(:never_update => true)
+
 end
